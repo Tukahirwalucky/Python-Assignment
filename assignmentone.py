9deec5c358c06ec551852b6d248124777e0824de
Tukahirwalucky <142319199+Tukahirwalucky@users.noreply.github.com>
2023-09-13 08:28:35 -0700

    assignmentone

---
+++ assignmentone.py
@@ -0,0 +1,34 @@
+# the sum of even numbers less than 8
+even_numbers_less_than_8= (2,4,6)
+sum_of_even_numbers = (2+4+6)
+print(sum_of_even_numbers)
+
+# the average of 5 numbers 
+r = 5
+s = 3
+t = 7
+u = 4
+v = 2
+total_number = (r + s + t + u + v)
+print(total_number)
+average = (total_number)/5
+print(type(average))
+print("The average is:" + str(average))
+
+# print the output of the following operations
+
+# -5+8*6
+result = (-5+8*6)
+print(result)
+
+#(55+9)%9
+result = (55+9)%9
+print(result)
+
+# 20+-3*5/8
+result = (20+-3*5/8)
+print(result)
+
+# 5+15/3*2-8%3
+result = (5+15/3*2-8%3)
+print(result)
